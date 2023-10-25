d1cb4834df55bd0c045b2d17e4836682285ec68d
Alejandro Barraza <abarraza.pk@gmail.com>
2023-10-24 23:23:53 -0300

    Añadiendo listas y tuplas (datos compuestos)

---
+++ Tipos de datos/datos-compuestos.py
@@ -0,0 +1,7 @@
+# Creando una lista (Es modificable)
+lista = ["Roberto", "Zuñiga", True, 1.60]
+
+# Creando una tupla (No es modificable)
+tupla = ("Roberto", "Zuñiga", True, 1.60)
+
+print(lista[1])
