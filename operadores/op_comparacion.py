cdb919c03014670f72f5f1b659a388c2d6344265
Alejandro Barraza <abarraza.pk@gmail.com>
2023-10-28 01:04:03 -0300

    Añadiendo apuntes de Operadores de Comparación

---
+++ operadores/op_comparacion.py
@@ -0,0 +1,15 @@
+# Los operadores de comparación nos devuelven True o False.
+
+igual_que = 5 == 6  # La comparación es FALSA así que nos devuelve False.
+
+distinto_de = 5 != 6  # la comparación es VERDADERA así que nos devuelve True.
+
+mayor_que = 5 > 6
+
+menor_que = 5 < 6
+
+mayor_o_igual = 5 >= 6
+
+menor_o_igual = 5 <= 6
+
+print(mayor_o_igual)
