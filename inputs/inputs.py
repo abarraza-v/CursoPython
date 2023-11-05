d45e909df9b00436032613b1aa97e69326f39f2a
Alejandro Barraza <abarraza.pk@gmail.com>
2023-11-04 21:57:16 -0300

    Añadiendo apuntes de Inputs

---
+++ inputs/inputs.py
@@ -0,0 +1,9 @@
+# Pedir un dato al usuario (Siempre se guarda un dato tipo String)
+numero = input("Ingresa un número: ")
+
+# Convertir el número a int/float para que realmente sirva para operaciones matemáticas
+
+resultado_float = float(numero) + 5.5
+
+# Mostrando el dato
+print(resultado_float)
