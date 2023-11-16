5c05b0409542c9cdb42ad0c5f1b3260534bc385c
Alejandro Barraza <abarraza.pk@gmail.com>
2023-11-16 11:03:12 -0300

    Fix: Apuntes de metodos diccionarios

--- Apuntes/Metodos/metodos_dict.py
+++ Apuntes/Metodos/metodos_dict.py
@@ -6,15 +6,15 @@ claves = diccionario.keys()
 # Devuelve un objeto dict_item iterable por un bucle
 diccionario_iterable = diccionario.items()
 
-# Obteniendo un elemento con get()
+# Obteniendo un elemento con get() (recibe como parámetro la key)
 # Si este método no encuentra nada, devuelve "None" en vez de parar el código con un error.
-valor_de_aaa = diccionario.get("aaa")
+valor_de_aaa = diccionario.get("nombre")
 
 # Eliminar un elemento del diccionario
 diccionario.pop("apellido")
 
 
-print(diccionario_iterable)
+print(valor_de_aaa)
 
 # Eliminar todo el diccionario
 diccionario.clear()
