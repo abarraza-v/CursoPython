55b1e81856db992c116f76b0c484bad9fb177a61
Alejandro Barraza <abarraza.pk@gmail.com>
2023-11-22 16:20:24 -0300

    sobreescribir_txt.py FIX

--- Apuntes/Archivos/sobreescribir_txt.py
+++ Apuntes/Archivos/sobreescribir_txt.py
@@ -2,7 +2,7 @@ with open("Apuntes\\Archivos\\texto.txt", "w", encoding="UTF-8") as archivo:
     # Sobreescribiendo el archivo (Se elimina el contenido previamente añadido).
     # archivo.write("He añadido esta linea con python!")
 
-    # Agregando 2 lineas con writelines(). (Es iterable)
+    # Sobreescribir 2 lineas con writelines(). (Es iterable)
     archivo.writelines(
         ["Hola, esta linea se añadido con writelines\n", "Aún no lo entiendo la verdad"]
     )
